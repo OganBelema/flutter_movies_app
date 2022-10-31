c3beee39ae50cd5ef4d1cf0ab4d996f6d032136a
Oganbelema <oganbelema@gmail.com>
2022-10-31 21:39:31 +0000

    customising row

--- lib/ui/movie_list_view.dart
+++ lib/ui/movie_list_view.dart
@@ -33,7 +33,8 @@ class _MovieListViewState extends State<MovieListView> {
       body: ListView.builder(
         itemCount: _movieList.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(
+          return movieCard(_movieList[index], context);
+          /*Card(
             elevation: 4.5,
             color: Colors.white,
             child: ListTile(
@@ -60,9 +61,46 @@ class _MovieListViewState extends State<MovieListView> {
                 );
               },
             ),
-          );
+          );*/
         },
       )
+    );
+  }
+
+  Widget movieCard(Movie movie, BuildContext context) {
+    return InkWell(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 120.0,
+        child: Card(
+          color: Colors.black45,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 54.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(movie.title),
+                    Text("Rating: ${movie.imdbRating} / 10"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Released: ${movie.released}"),
+                    Text(movie.runtime),
+                    Text(movie.rated)
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: () => debugPrint(movie.title),
     );
   }
 }
